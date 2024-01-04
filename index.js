872a9fc4950bd95ec73969b31dc044a7546b2556
Rodrigo Lopes <132340590+rodrigo-lourenco-lopes@users.noreply.github.com>
2024-01-04 11:32:06 +0100

    fix: remove current iteration

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const run = async () => {
     const number = Number(core.getInput('number'));
     const token = core.getInput('token');
     const iterationField = core.getInput('iteration-field'); // name of the iteration field
-    const iterationType = core.getInput('iteration'); // last or current
     const newiterationType = core.getInput('new-iteration'); // current or next
     const statuses = core.getInput('statuses').split(',');
 
@@ -18,13 +17,11 @@ const run = async () => {
     const lastIteration = projectData.fields.iteration.configuration.completedIterations[0];
     const currentIteration = projectData.fields.iteration.configuration.iterations[0];
     const nextIteration = projectData.fields.iteration.configuration.iterations[1];
-
-    const iteration = iterationType === 'last' ? lastIteration : currentIteration;
     const newIteration = newiterationType === 'current' ? currentIteration : nextIteration;
 
     const items = await project.items.list();
 
-    const filteredItems = items.filter(item => statuses.includes(item.fields.status) && item.fields.iteration === iteration.title);
+    const filteredItems = items.filter(item => statuses.includes(item.fields.status));
 
     await Promise.all(filteredItems.map(item => project.items.update(item.id, { iteration: newIteration.title })));
   } catch (error) {
